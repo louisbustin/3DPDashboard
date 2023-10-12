9bc649c629f10bc42f2e9f77a7fa7b4f596c1d2d
Louis Bustin <louis@eforge.us>
2023-10-11 19:04:56 -0500

    POST creates/updates now

--- backend/filament/POST/app.mjs
+++ backend/filament/POST/app.mjs
@@ -1,28 +1,42 @@
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { v4 as uuidv4 } from "uuid";
+const client = new DynamoDBClient({});
+const dynamo = DynamoDBDocumentClient.from(client);
+const tableName = "3dpdashboard_filament";
 
 export const lambdaHandler = async (event, context) => {
   try {
+    const requestJSON = JSON.parse(event.body);
+    const itemId = requestJSON.id || uuidv4();
+
+    await dynamo.send(
+      new PutCommand({
+        TableName: tableName,
+        Item: {
+          id: itemId,
+          name: requestJSON.name,
+          type: requestJSON.type,
+          brand: requestJSON.brand,
+        },
+      })
+    );
+
     return {
-      statusCode: 200,
+      statusCode: requestJSON.id ? 200 : 201,
       body: JSON.stringify({
-        message: "this will create a new filament",
-        event,
-        context,
+        message: requestJSON.id ? "updated" : "created",
+        id: itemId,
       }),
     };
   } catch (err) {
     console.log(err);
-    return err;
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "an error occurred",
+        error: err,
+      }),
+    };
   }
 };
