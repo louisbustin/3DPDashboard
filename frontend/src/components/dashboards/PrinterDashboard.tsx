54bc288702f4925ff693705fd1cf317adbac1d74
Louis Bustin <louis@eforge.us>
2024-10-19 18:07:19 +0000

    Resolve "Only pull first page of prints on printer page"

--- frontend/src/components/dashboards/PrinterDashboard.tsx
+++ frontend/src/components/dashboards/PrinterDashboard.tsx
@@ -13,6 +13,7 @@ import BarChartCard from "../BarChartCard";
 import usePrinters from "../../hooks/use-printers";
 import { IPrintLastEvaluatedKey } from "../../models/IPrintResponse";
 import PrintsGrid from "../grids/PrintsGrid";
+import BaseButton from "../buttons/BaseButton";
 
 const MAX_DATE = 9999999999999;
 const MIN_DATE = 0;
@@ -38,47 +39,50 @@ const PrinterDashboard = () => {
   const [minDateFilter, setMinDateFilter] = useState<number>(MIN_DATE);
   const [maxDateFilter, setMaxDateFilter] = useState<number>(MAX_DATE);
   const [prints, setPrints] = useState<IPrint[]>([]);
+  const [lastEvaluatedKey, setLastEvaluatedKey] = useState<
+    IPrintLastEvaluatedKey | undefined
+  >(undefined);
+  const [pullNextPrints, setPullNextPrints] = useState(true);
 
   useEffect(() => {
     const getPrintsByPrinter = async (printerId?: string) => {
       setShowLoadingDialog(true);
-      let evalKey: IPrintLastEvaluatedKey | undefined = undefined;
-      let accumulatedPrints: IPrint[] = [];
       let p;
 
       let response;
-      do {
-        response = await fetch(
-          apiURL +
-            printerId +
-            "/prints" +
-            (evalKey
-              ? `?LastEvaluatedKey=${encodeURIComponent(
-                  JSON.stringify(evalKey),
-                )}`
-              : ""),
-          {
-            headers: {
-              Authorization: `Bearer ${bearerToken}`,
-            },
+      response = await fetch(
+        apiURL +
+          printerId +
+          "/prints" +
+          (lastEvaluatedKey
+            ? `?LastEvaluatedKey=${encodeURIComponent(
+                JSON.stringify(lastEvaluatedKey)
+              )}`
+            : ""),
+        {
+          headers: {
+            Authorization: `Bearer ${bearerToken}`,
           },
-        );
-
-        p = await response.json();
-
-        if (p.data) {
-          accumulatedPrints = accumulatedPrints.concat(p.data);
         }
-        evalKey = { ...p.LastEvaluatedKey };
-      } while (p.LastEvaluatedKey);
-      setPrints([...accumulatedPrints]);
+      );
+
+      p = await response.json();
+
+      if (p.data) {
+        const accumulatedPrints = prints.concat(p.data);
+        setPrints([...accumulatedPrints]);
+        setPullNextPrints(false);
+      }
+      setLastEvaluatedKey(
+        p.LastEvaluatedKey ? { ...p.LastEvaluatedKey } : undefined
+      );
       setShowLoadingDialog(false);
     };
 
-    if (bearerToken) {
+    if (bearerToken && pullNextPrints) {
       getPrintsByPrinter(printerid);
     }
-  }, [bearerToken, printerid]);
+  }, [bearerToken, printerid, lastEvaluatedKey, pullNextPrints, prints]);
 
   useEffect(() => {
     if (data && !isLoading) {
@@ -108,7 +112,7 @@ const PrinterDashboard = () => {
   const onPieChartClick = (
     event: React.MouseEvent<SVGPathElement, MouseEvent>,
     itemIdentifier: PieItemIdentifier,
-    item: DefaultizedPieValueType,
+    item: DefaultizedPieValueType
   ) => {
     if (printFilter !== item.label) {
       setPrintFilter(item.label || "");
@@ -120,7 +124,7 @@ const PrinterDashboard = () => {
   const onDeletePrint = async (print: IPrint) => {
     setPrints((printArray) => {
       const newArray = printArray.filter(
-        (p) => p.insertedAt !== print.insertedAt,
+        (p) => p.insertedAt !== print.insertedAt
       );
       return [...newArray];
     });
@@ -260,7 +264,7 @@ const PrinterDashboard = () => {
                   onChange={
                     (v: Dayjs | null) =>
                       setMaxDateFilter(
-                        v ? v.unix() * 1000 + 86399999 : MAX_DATE,
+                        v ? v.unix() * 1000 + 86399999 : MAX_DATE
                       ) //Adding a day's (-1) amount of milliseconds to that the filters include that date
                   }
                   slotProps={{
@@ -270,11 +274,16 @@ const PrinterDashboard = () => {
                 />
               </Stack>
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={6} alignItems="center" justifyItems="center">
+              {lastEvaluatedKey && (
+                <BaseButton onClick={() => setPullNextPrints(true)}>
+                  Get Next 50 prints
+                </BaseButton>
+              )}
               {minDateFilter > 0 && (
                 <Chip
                   label={`Inserted after ${new Date(
-                    minDateFilter,
+                    minDateFilter
                   ).toLocaleDateString()}`}
                   onDelete={() => setMinDateFilter(0)}
                   sx={{ marginRight: 1 }}
@@ -283,7 +292,7 @@ const PrinterDashboard = () => {
               {maxDateFilter < MAX_DATE && (
                 <Chip
                   label={`Inserted before ${new Date(
-                    maxDateFilter,
+                    maxDateFilter
                   ).toLocaleDateString()}`}
                   onDelete={() => setMaxDateFilter(MAX_DATE)}
                   sx={{ marginRight: 1 }}
