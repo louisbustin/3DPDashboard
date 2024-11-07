ca986d70d3403e78aa8c1254c77c95cb5416e904
Louis Bustin <louis@eforge.us>
2024-11-07 02:09:25 +0000

    Resolve "Add ability to change inventory of filaments easily"

---
+++ frontend/src/components/grids/FilamentGrid.tsx
@@ -0,0 +1,216 @@
+import {PropsWithoutRef, useState} from "react";
+import IFilament, {FilamentStatus} from "../../models/IFilament";
+import {DataGrid, GridActionsCellItem, GridColDef} from "@mui/x-data-grid";
+import {Button, Checkbox, FormControlLabel, Stack, Tooltip} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import AddCircleOutline from "@mui/icons-material/AddCircleOutline";
+import RemoveCircleOutline from "@mui/icons-material/RemoveCircleOutline";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import useAPIToken from "../../hooks/use-api-token";
+import EditFilamentDrawer from "../drawers/EditFilamentDrawer";
+import _ from "lodash";
+import ConfirmationDialog from "../ConfirmationDialog";
+import LoadingDialog from "../LoadingDialog";
+import MessageBanner from "../MessageBanner";
+
+const apiURL = `${process.env.REACT_APP_API_BASE_URL}filament`;
+
+const FilamentGrid = (props: PropsWithoutRef<{
+  data: IFilament[],
+  onDeleteSuccess?: () => void,
+  onEditSuccess?: () => void,
+  allowDelete?: boolean,
+  allowEdit?: boolean
+  showSpoolAddRemove?: boolean
+}>) => {
+  const [selectedRowId, setSelectedRowId] = useState("");
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+  const [showInactive, setShowInactive] = useState(false);
+  const [showLoadingDialog, setShowLoadingDialog] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const bearerToken = useAPIToken();
+
+  const openDrawer = (id?: string) => {
+    setSelectedRowId(id || "");
+    setDrawerOpen(true);
+  };
+
+  const deleteFilament = async () => {
+    setDeleteDialogOpen(false);
+    setShowLoadingDialog(true);
+    const response = await fetch(`${apiURL}/${selectedRowId}`, {
+      method: "DELETE",
+      headers: {Authorization: `Bearer ${bearerToken}`},
+    });
+    if (response.ok) {
+      if (props.onDeleteSuccess) {
+        props.onDeleteSuccess();
+      }
+      setSuccessMessage("Filament successfully deleted.");
+      setTimeout(() => setSuccessMessage(""), 5000);
+    } else {
+      setErrorMessage("Error occurred deleting filament.");
+    }
+    setShowLoadingDialog(false);
+  };
+
+  const updateFilament = async (filament: IFilament) => {
+    setShowLoadingDialog(true);
+    const response = await fetch(apiURL, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${bearerToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(filament),
+    });
+    if (response.ok) {
+      if (props.onEditSuccess) {
+        props.onEditSuccess();
+      }
+      setSuccessMessage("Filament successfully updated.");
+      setTimeout(() => setSuccessMessage(""), 5000);
+    } else {
+      setErrorMessage("Error occurred updating filament.");
+    }
+    setShowLoadingDialog(false);
+  }
+
+  const addSpool = async (filament: IFilament) => {
+    filament.numberOfSpools = (filament.numberOfSpools || 0) + 1;
+    await updateFilament(filament);
+  }
+  const removeSpool = async (filament: IFilament) => {
+    filament.numberOfSpools = (filament.numberOfSpools || 0) - 1;
+    await updateFilament(filament);
+  }
+
+  const columns: GridColDef<IFilament>[] = [
+    {field: "brand", headerName: "Brand", flex: 1},
+    {field: "name", headerName: "Name", flex: 1},
+    {field: "type", headerName: "Type", flex: 1},
+    {field: "color", headerName: "Color", flex: 1},
+    {
+      field: "numberOfSpools",
+      headerName: "#Spools",
+      flex: 1,
+      valueFormatter: (params) => params.value || 0,
+      align: "center",
+      headerAlign: "center"
+    },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      cellClassName: "actions",
+      width: 150,
+      getActions: (params) => {
+        const actions = [];
+        if (props.showSpoolAddRemove) {
+          actions.push(<Tooltip title="Add Spool" enterDelay={1000}>
+            <GridActionsCellItem
+              icon={<AddCircleOutline/>}
+              label="Add Spool"
+              onClick={() => addSpool(params.row)}
+            />
+          </Tooltip>);
+
+          actions.push(<Tooltip title="Remove Spool" enterDelay={1000}>
+            <GridActionsCellItem
+              icon={<RemoveCircleOutline/>}
+              label="Remove Spool"
+              onClick={() => removeSpool(params.row)}
+              disabled={(params.row.numberOfSpools || 0) < 1}
+            />
+          </Tooltip>);
+        }
+        if (props.allowEdit) {
+          actions.push(<Tooltip title="Edit" enterDelay={1000}>
+            <GridActionsCellItem
+              icon={<EditIcon/>}
+              label="Edit"
+              onClick={() => openDrawer(params.row.id.toString())}
+            />
+          </Tooltip>)
+        }
+        if (props.allowDelete) {
+          actions.push(<Tooltip title="Delete" enterDelay={1000}>
+            <GridActionsCellItem
+              icon={<DeleteIcon/>}
+              label="Delete"
+              onClick={() => {
+                setSelectedRowId(params.row.id.toString());
+                setDeleteDialogOpen(true);
+              }}
+              color="inherit"
+            />
+          </Tooltip>)
+        }
+        return actions;
+      },
+    },
+  ];
+  return <>
+    <MessageBanner successMessage={successMessage} errorMessage={errorMessage}/>
+    <LoadingDialog open={showLoadingDialog}/>
+    <EditFilamentDrawer
+      open={drawerOpen}
+      onClose={async (updateOccurred) => {
+        if (updateOccurred) {
+          setShowLoadingDialog(true);
+          if (props.onEditSuccess) {
+            props.onEditSuccess();
+          }
+        }
+        setDrawerOpen(false);
+        setShowLoadingDialog(false);
+      }}
+      filamentId={_.clone(selectedRowId)}
+    />
+    <Stack direction="row" spacing={0.5} justifyContent="flex-end">
+      <FormControlLabel
+        control={
+          <Checkbox
+            onChange={(e) => setShowInactive(e.target.checked)}
+          ></Checkbox>
+        }
+        label="Show Inactive"
+      ></FormControlLabel>
+      <Button onClick={() => openDrawer()} aria-label="Add">
+        <AddIcon/>
+      </Button>
+    </Stack>
+    <ConfirmationDialog
+      open={deleteDialogOpen}
+      onCancel={() => setDeleteDialogOpen(false)}
+      onConfirm={() => deleteFilament()}
+      okText="Yes"
+      cancelText="No"
+      title="Are you sure?"
+    >
+      This will completely remove this filament from the database. Understand
+      that this operation is not reversible. Are you sure you want to delete
+      this filament?
+    </ConfirmationDialog>
+    <DataGrid
+      rows={props.data.filter((i) =>
+        !showInactive
+          ? i.filamentStatus === FilamentStatus.Active ||
+          i.filamentStatus === undefined
+          : true,
+      )}
+      columns={columns}
+      initialState={{
+        pagination: {
+          paginationModel: {page: 0, pageSize: 10},
+        },
+      }}
+      pageSizeOptions={[10, 50, 100]}
+    />
+  </>;
+};
+
+export default FilamentGrid;
--- frontend/src/pages/FilamentPage.tsx
+++ frontend/src/pages/FilamentPage.tsx
@@ -1,162 +1,22 @@
-import IFilament, { FilamentStatus } from "../models/IFilament";
-import { DataGrid, GridActionsCellItem, GridColDef } from "@mui/x-data-grid";
-import { useState } from "react";
-import {
-  Button,
-  Checkbox,
-  FormControlLabel,
-  Stack,
-  Tooltip,
-} from "@mui/material";
-import EditFilamentDrawer from "../components/drawers/EditFilamentDrawer";
 import LoadingDialog from "../components/LoadingDialog";
-import ConfirmationDialog from "../components/ConfirmationDialog";
-import MessageBanner from "../components/MessageBanner";
-import useAPIToken from "../hooks/use-api-token";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
-import AddIcon from "@mui/icons-material/Add";
 import useFilament from "../hooks/use-filament";
-import _ from "lodash";
-
-const apiURL = `${process.env.REACT_APP_API_BASE_URL}filament`;
+import FilamentGrid from "../components/grids/FilamentGrid";
 
 const FilamentPage = () => {
-  const { filament: data, refresh, isLoading } = useFilament();
-  const [selectedRowId, setSelectedRowId] = useState("");
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
-  const [showLoadingDialog, setShowLoadingDialog] = useState(false);
-  const [successMessage, setSuccessMessage] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [showInactive, setShowInactive] = useState(false);
-  const bearerToken = useAPIToken();
+  const {filament: data, refresh, isLoading} = useFilament();
 
-  const openDrawer = (id?: string) => {
-    setSelectedRowId(id || "");
-    setDrawerOpen(true);
-  };
-
-  const deleteFilament = async () => {
-    setDeleteDialogOpen(false);
-    setShowLoadingDialog(true);
-    const response = await fetch(`${apiURL}/${selectedRowId}`, {
-      method: "DELETE",
-      headers: { Authorization: `Bearer ${bearerToken}` },
-    });
-    if (response.ok) {
-      refresh();
-      setSuccessMessage("Filament successfully deleted.");
-      setTimeout(() => setSuccessMessage(""), 5000);
-    } else {
-      setErrorMessage("Error occurred deleting filament.");
-    }
-    setShowLoadingDialog(false);
-  };
-  const columns: GridColDef<IFilament>[] = [
-    { field: "brand", headerName: "Brand", flex: 1 },
-    { field: "name", headerName: "Name", flex: 1 },
-    { field: "type", headerName: "Type", flex: 1 },
-    { field: "color", headerName: "Color", flex: 1 },
-    {
-      field: "actions",
-      type: "actions",
-      headerName: "Actions",
-      cellClassName: "actions",
-      width: 150,
-      getActions: ({ id }) => {
-        return [
-          <Tooltip title="Edit" enterDelay={1000}>
-            <GridActionsCellItem
-              icon={<EditIcon />}
-              label="Edit"
-              onClick={() => openDrawer(id.toString())}
-            />
-          </Tooltip>,
-          <Tooltip title="Delete" enterDelay={1000}>
-            <GridActionsCellItem
-              icon={<DeleteIcon />}
-              label="Delete"
-              onClick={() => {
-                setSelectedRowId(id.toString());
-                setDeleteDialogOpen(true);
-              }}
-              color="inherit"
-            />
-          </Tooltip>,
-        ];
-      },
-    },
-  ];
   return (
     <>
-      <MessageBanner
-        successMessage={successMessage}
-        errorMessage={errorMessage}
-        onClose={() => {
-          setSuccessMessage("");
-          setErrorMessage("");
-        }}
-      ></MessageBanner>
-      <EditFilamentDrawer
-        open={drawerOpen}
-        onClose={async (updateOccurred) => {
-          if (updateOccurred) {
-            setShowLoadingDialog(true);
-            refresh();
-          }
-          setDrawerOpen(false);
-          setShowLoadingDialog(false);
-        }}
-        filamentId={_.clone(selectedRowId)}
-      />
-
-      <ConfirmationDialog
-        open={deleteDialogOpen}
-        onCancel={() => setDeleteDialogOpen(false)}
-        onConfirm={() => deleteFilament()}
-        okText="Yes"
-        cancelText="No"
-        title="Are you sure?"
-      >
-        This will completely remove this filament from the database. Understand
-        that this operation is not reversible. Are you sure you want to delete
-        this filament?
-      </ConfirmationDialog>
-
       <h2>Filaments</h2>
-      <LoadingDialog open={isLoading || showLoadingDialog}></LoadingDialog>
-      <Stack direction="row" spacing={0.5} justifyContent="flex-end">
-        <FormControlLabel
-          control={
-            <Checkbox
-              onChange={(e) => setShowInactive(e.target.checked)}
-            ></Checkbox>
-          }
-          label="Show Inactive"
-        ></FormControlLabel>
-        <Button onClick={() => openDrawer()} aria-label="Add">
-          <AddIcon />
-        </Button>
-      </Stack>
-
-      {data && (
-        <DataGrid
-          rows={data.filter((i) =>
-            !showInactive
-              ? i.filamentStatus === FilamentStatus.Active ||
-                i.filamentStatus === undefined
-              : true,
-          )}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 10 },
-            },
-          }}
-          pageSizeOptions={[10, 50, 100]}
-        />
-      )}
+      <LoadingDialog open={isLoading}></LoadingDialog>
+      <FilamentGrid
+        data={data || []}
+        onDeleteSuccess={() => refresh()}
+        onEditSuccess={() => refresh()}
+        allowDelete
+        allowEdit
+        showSpoolAddRemove
+      />
     </>
   );
 };
